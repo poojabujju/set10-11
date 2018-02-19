e410863204cc658699be448794af398f8e6bf5af
poojabujju <31477611+poojabujju@users.noreply.github.com>
2018-02-19 16:25:24 +0530

    Create 103.c

---
+++ 103.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    char ch[100];
+    int i;
+    printf("enter the string");
+    scanf("%[^\n]s",&ch);
+    ch[0]=toupper(ch[0]);
+    for(i=0;ch[i]!='\0';i++)
+    {
+        if(ch[i]==' ')
+        {
+            ch[i+1]=toupper(ch[i+1]);
+        }
+    }
+    printf("%s",ch);
+}
