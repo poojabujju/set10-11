1c6474c8e5d988839012086bb54a5792619cee91
poojabujju <31477611+poojabujju@users.noreply.github.com>
2018-02-19 16:30:25 +0530

    Create 104.c

---
+++ 104.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int a,n,k;
+printf("\nEnter a Number : ");
+scanf("%d",&n);
+printf("\nEnter its power : ");
+scanf("%d",&k);
+a=pow(n,k);
+printf("\n%d",a);
+}
